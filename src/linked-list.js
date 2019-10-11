f9d27ea4071eb227d80b629d2a81e3bd84f19191
Kirill Mager <kiruha24@mail.ru>
2019-10-12 01:34:39 +0300

    feat: add new method at(index)

--- src/linked-list.js
+++ src/linked-list.js
@@ -6,14 +6,41 @@ class LinkedList {
         this._tail = new Node();
         this.length = 0;
     }
+    //Node это элемент list
+    append(data) { 
+        
+        if (this.length === 0) {
+            this._head = new Node(data);
+            this._tail = new Node(data);
+            this.length++;
+        } else {
+            this._tail = this._head;
+            while(this._tail.next !== null) {
+                this._tail = this._tail.next;
+            }
+            this._tail.next = new Node(data);
+            this._tail.next.prev = this._tail;
+            this._tail = this._tail.next;
+            this.length++;
+        }
+    } 
 
-    append(data) {}
+    head() {
+        return this._head.data;
+    }
 
-    head() {}
+    tail() {
+        return this._tail.data;
+    }
 
-    tail() {}
-
-    at(index) {}
+    at(index) {
+        let currentNode = this._head;
+        while (index != 0) {
+            currentNode = currentNode.next;
+            index--;
+        }
+        return currentNode.data;
+    }
 
     insertAt(index, data) {}
 
